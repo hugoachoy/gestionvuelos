1fb420e4e89d7778ebe529fe4bae3c845a9f79bb
Hugo Choy <hugoachoy@gmail.com>
2025-06-20 22:14:40 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- src/components/common/delete-dialog.tsx
+++ src/components/common/delete-dialog.tsx
@@ -41,6 +41,6 @@ export function DeleteDialog({ open, onOpenChange, onConfirm, itemName = "este e
           </AlertDialogAction>
         </AlertDialogFooter>
       </AlertDialogContent>
-    </AlertDialog> // Corrected closing tag
+    </AlertDialog>
   );
 }
